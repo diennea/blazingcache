6a45ee2134d7551ce611e00d52a562bd81481bf3
eolivelli <eolivelli@gmail.com>
2015-12-03 07:38:08 +0100

    fix evict

--- blazingcache-core/src/test/java/blazingcache/SimpleEvictMaxMemoryTest.java
+++ blazingcache-core/src/test/java/blazingcache/SimpleEvictMaxMemoryTest.java
@@ -33,37 +33,75 @@ public class SimpleEvictMaxMemoryTest {
                 client1.start();
                 assertTrue(client1.waitForConnection(10000));
 
-                client1.put("pippo1", data, 0);
-                client1.put("pippo2", data, 0);
-                client1.put("pippo3", data, 0);
-                client1.put("pippo4", data, 0);
-                client1.put("pippo5", data, 0);
-                assertEquals(40, client1.getActualMemory());
+                {
+                    client1.put("pippo1", data, 0);
+                    client1.put("pippo2", data, 0);
+                    client1.put("pippo3", data, 0);
+                    client1.put("pippo4", data, 0);
+                    client1.put("pippo5", data, 0);
+                    assertEquals(40, client1.getActualMemory());
 
-                Assert.assertArrayEquals(data, client1.get("pippo1").getSerializedData());
-                Assert.assertArrayEquals(data, client1.get("pippo2").getSerializedData());
-                Assert.assertArrayEquals(data, client1.get("pippo3").getSerializedData());
-                Assert.assertArrayEquals(data, client1.get("pippo5").getSerializedData());
-                Thread.sleep(100);
-                Assert.assertArrayEquals(data, client1.get("pippo4").getSerializedData()); // last get, questo verrà tenuto
+                    Assert.assertArrayEquals(data, client1.get("pippo1").getSerializedData());
+                    Assert.assertArrayEquals(data, client1.get("pippo2").getSerializedData());
+                    Assert.assertArrayEquals(data, client1.get("pippo3").getSerializedData());
+                    Assert.assertArrayEquals(data, client1.get("pippo5").getSerializedData());
+                    Thread.sleep(100);
+                    Assert.assertArrayEquals(data, client1.get("pippo4").getSerializedData()); // last get, questo verrà tenuto
 
-                client1.setMaxMemory(10);
+                    client1.setMaxMemory(10);
 
-                for (int i = 0; i < 100; i++) {
-                    System.out.println("client1.getActualMemory():" + client1.getActualMemory());
-                    if (client1.getActualMemory() == 8) {
-                        break;
+                    for (int i = 0; i < 100; i++) {
+                        System.out.println("client1.getActualMemory():" + client1.getActualMemory());
+                        if (client1.getActualMemory() == 8) {
+                            break;
+                        }
+                        Thread.sleep(1000);
                     }
-                    Thread.sleep(1000);
+
+                    assertEquals(8, client1.getActualMemory());
+                    assertNull(client1.get("pippo1"));
+                    assertNull(client1.get("pippo2"));
+                    assertNull(client1.get("pippo3"));
+                    assertNull(client1.get("pippo5"));
+                    Assert.assertArrayEquals(data, client1.get("pippo4").getSerializedData());
                 }
+                client1.invalidateByPrefix("");
 
-                assertEquals(8, client1.getActualMemory());
-                assertNull(client1.get("pippo1"));
-                assertNull(client1.get("pippo2"));
-                assertNull(client1.get("pippo3"));
-                assertNull(client1.get("pippo5"));
-                Assert.assertArrayEquals(data, client1.get("pippo4").getSerializedData());
+                assertEquals(0, client1.getActualMemory());
 
+                {
+                    client1.put("pluto1", data, 0);
+                    client1.put("pluto2", data, 0);
+                    client1.put("pluto3", data, 0);
+                    client1.put("pluto4", data, 0);
+                    client1.put("pluto5", data, 0);
+                    assertEquals(40, client1.getActualMemory());
+
+                    Assert.assertArrayEquals(data, client1.get("pluto1").getSerializedData());
+                    Assert.assertArrayEquals(data, client1.get("pluto2").getSerializedData());
+                    Assert.assertArrayEquals(data, client1.get("pluto3").getSerializedData());
+                    Assert.assertArrayEquals(data, client1.get("pluto5").getSerializedData());
+                    Thread.sleep(100);
+                    Assert.assertArrayEquals(data, client1.get("pluto4").getSerializedData()); // last get, questo verrà tenuto
+
+                    client1.setMaxMemory(10);
+
+                    for (int i = 0; i < 100; i++) {
+                        System.out.println("client1.getActualMemory():" + client1.getActualMemory());
+                        if (client1.getActualMemory() == 8) {
+                            break;
+                        }
+                        Thread.sleep(1000);
+                    }
+
+                    assertEquals(8, client1.getActualMemory());
+                    assertNull(client1.get("pluto1"));
+                    assertNull(client1.get("pluto2"));
+                    assertNull(client1.get("pluto3"));
+                    assertNull(client1.get("pluto5"));
+                    Assert.assertArrayEquals(data, client1.get("pluto4").getSerializedData());
+                }
+                
             }
 
         }
@@ -78,9 +116,9 @@ public class SimpleEvictMaxMemoryTest {
         try (CacheServer cacheServer = new CacheServer("ciao", serverHostData)) {
             cacheServer.start();
             try (CacheClient client1 = new CacheClient("theClient1", "ciao", new NettyCacheServerLocator(serverHostData));
-                     CacheClient client2 = new CacheClient("theClient2", "ciao", new NettyCacheServerLocator(serverHostData));) {
+                    CacheClient client2 = new CacheClient("theClient2", "ciao", new NettyCacheServerLocator(serverHostData));) {
                 client1.start();
-                client2.start();                
+                client2.start();
                 assertTrue(client1.waitForConnection(10000));
                 assertTrue(client2.waitForConnection(10000));
 
@@ -89,12 +127,12 @@ public class SimpleEvictMaxMemoryTest {
                 client2.put("pippo3", data, 0);
                 client2.put("pippo4", data, 0);
                 client2.put("pippo5", data, 0);
-                
+
                 client1.fetch("pippo1");
                 client1.fetch("pippo2");
                 client1.fetch("pippo3");
                 client1.fetch("pippo4");
-                client1.fetch("pippo5");                
+                client1.fetch("pippo5");
                 assertEquals(40, client1.getActualMemory());
 
                 Assert.assertArrayEquals(data, client1.get("pippo1").getSerializedData());
