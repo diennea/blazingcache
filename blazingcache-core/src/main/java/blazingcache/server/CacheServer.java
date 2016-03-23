cac4649b90c93caa34e4ed8287a7e5028c116c7e
eolivelli <eolivelli@gmail.com>
2016-03-23 11:02:27 +0100

    1.6.1 release

--- blazingcache-core/src/main/java/blazingcache/server/CacheServer.java
+++ blazingcache-core/src/main/java/blazingcache/server/CacheServer.java
@@ -75,7 +75,7 @@ public class CacheServer implements AutoCloseable {
     private long clientFetchTimeout = 2000;
 
     public static String VERSION() {
-        return "1.6.1-BETA5";
+        return "1.6.1";
     }
 
     public CacheServer(String sharedSecret, ServerHostData serverHostData) {
