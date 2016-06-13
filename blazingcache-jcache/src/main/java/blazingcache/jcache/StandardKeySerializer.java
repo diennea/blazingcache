96911d659f60887f4d1e80864fba9cddc784467a
eolivelli <eolivelli@gmail.com>
2016-06-13 10:06:19 +0200

    BLAZ-40

--- blazingcache-jcache/src/main/java/blazingcache/jcache/StandardKeySerializer.java
+++ blazingcache-jcache/src/main/java/blazingcache/jcache/StandardKeySerializer.java
@@ -21,7 +21,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Base64;
+import java.util.UUID;
 import javax.cache.CacheException;
+import org.jboss.netty.util.internal.ConcurrentWeakKeyHashMap;
 
 /**
  * Standard keys serializer
@@ -30,10 +32,13 @@ import javax.cache.CacheException;
  */
 public class StandardKeySerializer implements Serializer<Object, String> {
 
+    private final ConcurrentWeakKeyHashMap<Object, String> notSerializableKeys = new ConcurrentWeakKeyHashMap<>();
+
     @Override
     public String serialize(Object value) {
         if (value instanceof String) {
             // BlazingCache is made for string keys!
+            // if you are not using strings then implement a custom serializer
             String key = (String) value;
             if (!key.startsWith("$")) {
                 return key;
@@ -44,7 +49,16 @@ public class StandardKeySerializer implements Serializer<Object, String> {
             ObjectOutputStream o = new ObjectOutputStream(oo);
             o.writeUnshared(value);
             o.flush();
-            return "$"+Base64.getEncoder().encodeToString(oo.toByteArray());
+            return "$" + Base64.getEncoder().encodeToString(oo.toByteArray());
+        } catch (java.io.NotSerializableException notSerializable) {
+            // VERY BAD CASE (the TCK for instance), Usually keys are serializable or the client uses a custom serializer
+            String alreadyCached = notSerializableKeys.get(value);
+            if (alreadyCached != null) {
+                return alreadyCached;
+            }
+            String id = value.getClass().getName() + "_" + System.identityHashCode(value);
+            notSerializableKeys.put(value, id);
+            return id;
         } catch (IOException err) {
             throw new CacheException(err);
         }
@@ -54,6 +68,10 @@ public class StandardKeySerializer implements Serializer<Object, String> {
     public Object deserialize(String keyOnCacheClient) {
         if (!keyOnCacheClient.startsWith("$")) {
             return keyOnCacheClient;
+        }
+        Object notSerializable = notSerializableKeys.get(keyOnCacheClient);
+        if (notSerializable != null) {
+            return notSerializable;
         }
         try {
             byte[] decoded = Base64.getDecoder().decode(keyOnCacheClient.substring(1));
