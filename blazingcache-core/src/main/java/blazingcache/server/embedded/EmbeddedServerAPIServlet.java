12c1de214f012366ebca662cb3c8f86265aebbb4
dennis.mercuriali <dennis.mercuriali@diennea.com>
2024-01-03 18:06:45 +0100

    Revert "Task/#190 jakarta (#191)"
    This reverts commit 7565bab596b6e8ea791e94f49745d3ecd62fd7a3.

--- blazingcache-core/src/main/java/blazingcache/server/embedded/EmbeddedServerAPIServlet.java
+++ blazingcache-core/src/main/java/blazingcache/server/embedded/EmbeddedServerAPIServlet.java
@@ -20,10 +20,10 @@
 package blazingcache.server.embedded;
 
 import java.io.IOException;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet API Client
